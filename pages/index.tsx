f39dbc753b8eabd28f84fe26ef39f5ecbd1c5451
Dickinson Arismendy <darismendy@apricoot.com>
2021-06-14 21:11:32 -0500

    feat: first commit with typescript

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
+import { AppProps } from 'next/dist/next-server/lib/router/router'
 import Head from 'next/head'
 
-export default function Home() {
+export default function Home({Component, pageProps}: AppProps) {
   return (
     <div className="container">
       <Head>
@@ -10,7 +11,7 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Next.js!!!</a>
         </h1>
 
         <p className="description">
