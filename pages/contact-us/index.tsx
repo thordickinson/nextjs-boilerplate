67527d929b571600cccce5f6ed6ae8871af6566a
Oscar Ramos <64324454+lacroix021@users.noreply.github.com>
2021-12-08 05:49:46 -0500

    added form contact in progress

--- pages/contact-us/index.tsx
+++ pages/contact-us/index.tsx
@@ -1,13 +1,18 @@
 import styles from "./styles.module.scss"
 import DefaultLayout from "../../components/layouts/default-layout"
 import { Formik } from "formik"
+import React, {useState} from "react"
+
 
 export default function Contact_Us() {
+const [formSend, setFormSend] = useState(false)
+
+
+
     return (
         <DefaultLayout>
             <div className={styles.contact_section}>
                 <div className={styles.map_background}></div>
-
                 <div className={styles.container}>
                     <div className={styles.contact_wrap}>
                         <div className={styles.itemA}>
@@ -32,7 +37,84 @@ export default function Contact_Us() {
                             </div>
                         </div>
                         <div className={styles.itemB}>
-                            {/*aqui poner el formulario de formik */}
+                            <Formik
+                                initialValues={{
+                                name:'',
+                                email:'',
+                                message:''
+                                }}
+                                validate={(values)=>{
+                                    let errores = {};
+                                    //validacion nombre
+                                    if(!values.name){
+                                        errores.name = "por favor ingresa un nombre";
+                                    }else if(!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(values.name)){
+                                        errores.name = "El nombre solo puede contener letras y espacios";
+                                    }
+
+                                    //validacion mail
+                                    if(!values.email){
+                                        errores.email = "por favor ingresa un correo electronico";
+                                    }else if(!/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(values.email)){
+                                        errores.email = "El correo solo puede contener letras, numeros, puntos, guiones y guion bajo";
+                                    }
+                                    return errores;
+                                }}
+                                onSubmit={(values, {resetForm})=>{
+                                    resetForm();
+                                    console.log("formulario enviado");
+                                    setFormSend(true);
+                                    setTimeout(() =>setFormSend(false), 5000);
+                                }}
+                            >
+                                {( {values, errors, touched, handleSubmit, handleChange, handleBlur}) =>(
+                                    <form className={styles.containerItems} onSubmit={handleSubmit}>
+                                        {console.log(errors)}
+                                        <div className={styles.inputNameCont}>
+                                            <input 
+                                                type="text" 
+                                                id="name" 
+                                                name="name" 
+                                                placeholder="your name"
+                                                value={values.name}
+                                                onChange={handleChange}
+                                                onBlur={handleBlur}
+                                                className={styles.inputName}
+                                            />
+                                            {touched.name && errors.name && <div className={styles.error}>{errors.name}</div>}
+                                        </div>
+                                        <div className={styles.inputEmailCont}>
+                                            <input 
+                                                type="text" 
+                                                id="email" 
+                                                name="email" 
+                                                placeholder="correo@correo.com" 
+                                                value={values.email}
+                                                onChange={handleChange}
+                                                onBlur={handleBlur}
+                                                className={styles.inputEmail}
+                                            />
+                                            {touched.email && errors.email && <div className={styles.error}>{errors.email}</div>}
+                                        </div>
+                                        <div className={styles.inputMessageCont}>
+                                            <input 
+                                                type="text" 
+                                                id="message" 
+                                                name="message" 
+                                                placeholder="message..."
+                                                value={values.message}
+                                                onChange={handleChange}
+                                                onBlur={handleBlur}
+                                                className={styles.inputMessage}
+                                            />
+                                        </div>
+                                        <div className={styles.buttonForm}>
+                                            <button type="submit">Send Message</button>
+                                            {formSend && <p className={styles.exito}>Formulario enviado con exito!</p>}
+                                        </div>
+                                     </form>
+                                 )}
+                            </Formik>
                         </div>
                     </div>
                 </div>
