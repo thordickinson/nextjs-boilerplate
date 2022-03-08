df5db2baf34326f7bdbeeb1900575c4959a156c0
Dickinson Arismendy <thordickinson@gmail.com>
2022-03-07 22:09:31 -0500

    fix: eslint and removed some references to next-auth

--- pages/blog/[page].tsx
+++ pages/blog/[page].tsx
@@ -1,69 +1,86 @@
-import { useRouter } from 'next/router'
-import { GetStaticPropsResult, GetStaticPropsContext, GetStaticPathsContext, GetStaticPathsResult } from 'next'
-import DefaultLayout from '../../components/layouts/default-layout'
-import BlogEntryCard from '../../components/blog/entry-card/entry-card'
-import PageHeader from '../../components/common/page-header/page-header'
-import AsideContentSection from '../../components/common/aside-content-section/aside-content-section'
-import styles from "./[page].module.scss"
-import PaginatorComponent from '../../components/common/paginator/paginator'
-import { PagedResult } from '../../utils/pagination'
-import TagsWidget from '../../components/blog/tags-widget/tags-widget'
-import RecentPostsWidgets from '../../components/blog/recent-widget/recent-widget'
-import { useSession } from 'next-auth/client'
+import { useRouter } from "next/router";
+import {
+  GetStaticPropsResult,
+  GetStaticPropsContext,
+  GetStaticPathsContext,
+  GetStaticPathsResult,
+} from "next";
+import DefaultLayout from "../../components/layouts/default-layout";
+import BlogEntryCard from "../../components/blog/entry-card/entry-card";
+import PageHeader from "../../components/common/page-header/page-header";
+import AsideContentSection from "../../components/common/aside-content-section/aside-content-section";
+import styles from "./[page].module.scss";
+import PaginatorComponent from "../../components/common/paginator/paginator";
+import { PagedResult } from "../../utils/pagination";
+import TagsWidget from "../../components/blog/tags-widget/tags-widget";
+import RecentPostsWidgets from "../../components/blog/recent-widget/recent-widget";
 
-
-const PAGE_SIZE = 6
-const REVALIDATION = 60 * 60 //1 hour
+const PAGE_SIZE = 6;
+const REVALIDATION = 60 * 60; //1 hour
 
 export default function PostList({ posts, tags, recent }) {
-    const [ session, loading ] = useSession()
-
-    const asideContent = <div>
-        {session?.user?.email}
-        <TagsWidget tags={tags}></TagsWidget>
-        <RecentPostsWidgets recent={recent}></RecentPostsWidgets>
+  const asideContent = (
+    <div>
+      <TagsWidget tags={tags}></TagsWidget>
+      <RecentPostsWidgets recent={recent}></RecentPostsWidgets>
     </div>
-    return <DefaultLayout>
-        <PageHeader sectionTitle="Blog" subtitle="This is the subtitle of the blog" ></PageHeader>
-        <AsideContentSection aside={asideContent}>
-            <div className={styles.postList}>
-                {posts.content.map(p => <div key={p._id} className={styles.entryCard}>
-                    <BlogEntryCard entry={p}></BlogEntryCard>
-                </div>)}
+  );
+  return (
+    <DefaultLayout>
+      <PageHeader
+        sectionTitle="Blog"
+        subtitle="This is the subtitle of the blog"
+      ></PageHeader>
+      <AsideContentSection aside={asideContent}>
+        <div className={styles.postList}>
+          {posts.content.map((p) => (
+            <div key={p._id} className={styles.entryCard}>
+              <BlogEntryCard entry={p}></BlogEntryCard>
             </div>
-            <PaginatorComponent baseLink="/blog" page={posts}></PaginatorComponent>
-        </AsideContentSection>
+          ))}
+        </div>
+        <PaginatorComponent baseLink="/blog" page={posts}></PaginatorComponent>
+      </AsideContentSection>
     </DefaultLayout>
-
+  );
 }
 
-export async function getStaticPaths(ctx: GetStaticPathsContext): Promise<GetStaticPathsResult> {
-    const res = await fetch(`${process.env.BACKEND_URL}/api/blog/posts?limit=1`)
-    const response: PagedResult<any> = await res.json()
-    const pageCount = Math.ceil(response.totalElements / PAGE_SIZE)
-    const paths = []
-    for (let i = 0; i < pageCount; i++) paths.push({ params: { page: `${(i + 1)}` } })
-    return {
-        paths,
-        fallback: 'blocking'
-    }
+export async function getStaticPaths(
+  ctx: GetStaticPathsContext
+): Promise<GetStaticPathsResult> {
+  const res = await fetch(`${process.env.BACKEND_URL}/api/blog/posts?limit=1`);
+  const response: PagedResult<any> = await res.json();
+  const pageCount = Math.ceil(response.totalElements / PAGE_SIZE);
+  const paths = [];
+  for (let i = 0; i < pageCount; i++)
+    paths.push({ params: { page: `${i + 1}` } });
+  return {
+    paths,
+    fallback: "blocking",
+  };
 }
 
-export async function getStaticProps(ctx: GetStaticPropsContext): Promise<GetStaticPropsResult<any>> {
-    const { page = 1 } = ctx.params || {}
-    const res = await fetch(`${process.env.BACKEND_URL}/api/blog/posts?limit=6&sort=-createdAt&page=${page}`)
-    const posts = await res.json()
+export async function getStaticProps(
+  ctx: GetStaticPropsContext
+): Promise<GetStaticPropsResult<any>> {
+  const { page = 1 } = ctx.params || {};
+  const res = await fetch(
+    `${process.env.BACKEND_URL}/api/blog/posts?limit=6&sort=-createdAt&page=${page}`
+  );
+  const posts = await res.json();
 
-    const tagsResponse = await fetch(`${process.env.BACKEND_URL}/api/blog/tags`)
-    const tagPage = await tagsResponse.json()
-    const tags = tagPage.content
+  const tagsResponse = await fetch(`${process.env.BACKEND_URL}/api/blog/tags`);
+  const tagPage = await tagsResponse.json();
+  const tags = tagPage.content;
 
-    const recentResponse = await fetch(`${process.env.BACKEND_URL}/api/blog/posts?sort=-creacion&limit=4`)
-    const recentPage = await recentResponse.json()
-    const recent = recentPage.content
+  const recentResponse = await fetch(
+    `${process.env.BACKEND_URL}/api/blog/posts?sort=-creacion&limit=4`
+  );
+  const recentPage = await recentResponse.json();
+  const recent = recentPage.content;
 
-    return {
-        props: { posts, tags, recent },
-        revalidate: REVALIDATION 
-    }
+  return {
+    props: { posts, tags, recent },
+    revalidate: REVALIDATION,
+  };
 }
--- pages/user/profile/index.tsx
+++ pages/user/profile/index.tsx
@@ -1,11 +1,11 @@
-import DashboardTitle from '../../../components/dashboard/title'
-import DashboardLayout from '../../../components/layouts/dashboard-layout'
-import styles from './styles.module.scss'
-import React, { useState, useEffect } from 'react'
-import DashboardCard from '../../../components/dashboard/card'
-import { getUser } from '../../../utils/auth'
-import DashboardCardHeader from '../../../components/dashboard/card-header'
-import DashboardCardHeaderButton from '../../../components/dashboard/card-header-button'
+import DashboardTitle from "../../../components/dashboard/title";
+import DashboardLayout from "../../../components/layouts/dashboard-layout";
+import styles from "./styles.module.scss";
+import React, { useState, useEffect } from "react";
+import DashboardCard from "../../../components/dashboard/card";
+import { getUser } from "../../../utils/auth";
+import DashboardCardHeader from "../../../components/dashboard/card-header";
+import DashboardCardHeaderButton from "../../../components/dashboard/card-header-button";
 import { Auth } from "aws-amplify";
 import { Formik, Form } from "formik";
 import FormikControl from "../../../components/formik-control/FormikControl";
@@ -14,190 +14,216 @@ import { Button } from "antd";
 import * as Yup from "yup";
 
 export default function UserProfile() {
+  const [user, setUser] = useState(null);
 
-    const [user, setUser] = useState(null);
+  const [currentName, setCurrentName] = useState(undefined);
+  const [currentLastName, setCurrentLastName] = useState(undefined);
+  const [currentBirthdate, setCurrentBirthdate] = useState(undefined);
+  const [currentPhone, setCurrentPhone] = useState(undefined);
 
-    const [currentName, setCurrentName] = useState(undefined);
-    const [currentLastName, setCurrentLastName] = useState(undefined);
-    const [currentBirthdate, setCurrentBirthdate] = useState(undefined);
-    const [currentPhone, setCurrentPhone] = useState(undefined);
+  const [updateField, setUpdateField] = useState(false);
 
-    const [updateField, setUpdateField] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-    const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    (async () => {
+      const u = await getUser();
+      setUser(u);
+      getUserInfo();
+    })();
+  }, [updateField]);
 
-    useEffect(() => {
-        (async () => {
-            const u = await getUser()
-            setUser(u)
-            getUserInfo();
-        })()
-    }, [updateField])
+  //test
+  function SetCurrentUser(username, lastname, birthdate, phone) {
+    setCurrentName(username);
+    setCurrentLastName(lastname);
+    setCurrentBirthdate(birthdate);
+    setCurrentPhone(phone);
+  }
 
+  //test
+  async function getUserInfo() {
+    const user = await Auth.currentAuthenticatedUser();
+    const thisUser = user.attributes;
+    SetCurrentUser(
+      thisUser.given_name,
+      thisUser.family_name,
+      thisUser.birthdate,
+      thisUser.phone_number
+    );
+    //console.log('attributes:', user.attributes);
+  }
 
-    //test
-    function SetCurrentUser(username, lastname, birthdate, phone) {
-        setCurrentName(username);
-        setCurrentLastName(lastname);
-        setCurrentBirthdate(birthdate);
-        setCurrentPhone(phone);
-    }
+  async function updateUser(name, lastname, birthdate, phone) {
+    const user = await Auth.currentAuthenticatedUser();
+    await Auth.updateUserAttributes(user, {
+      family_name: lastname,
+      given_name: name,
+      birthdate: birthdate,
+      phone_number: phone,
+    });
+  }
 
-    //test
-    async function getUserInfo() {
-        const user = await Auth.currentAuthenticatedUser();
-        const thisUser = user.attributes;
-        SetCurrentUser(thisUser.given_name, thisUser.family_name, thisUser.birthdate, thisUser.phone_number);
-        //console.log('attributes:', user.attributes);
-    }
+  const initialValues = {
+    name: currentName,
+    lastname: currentLastName,
+    birthdate: currentBirthdate,
+    phone: currentPhone,
+  };
 
-    async function updateUser(name, lastname, birthdate, phone) {
-        const user = await Auth.currentAuthenticatedUser();
-        await Auth.updateUserAttributes(user ,{
-          'family_name': lastname,
-          'given_name' : name,
-          'birthdate': birthdate,
-          'phone_number': phone
-        });
-    }
-
-
-    const initialValues = {
-        name: currentName,
-        lastname:currentLastName,
-        birthdate:currentBirthdate,
-        phone:currentPhone
-      };
-    
-    const validationSchema = Yup.object({
+  const validationSchema = Yup.object({
     name: Yup.string().required("update your name"),
     lastname: Yup.string().required("Update your Lastname"),
     birthdate: Yup.date().required("Update your Birthdate"),
-    phone: Yup.string().required("Update your phone number")
-    });
-    
-    const onSubmit = (values, { resetForm }) => {
-        setLoading(true);
-        updateUser(values.name, values.lastname, values.birthdate, values.phone)
-        .then(() => {
+    phone: Yup.string().required("Update your phone number"),
+  });
+
+  const onSubmit = (values, { resetForm }) => {
+    setLoading(true);
+    updateUser(values.name, values.lastname, values.birthdate, values.phone)
+      .then(() => {
         setUpdateField(false);
         toast.success("Data Updated");
         setLoading(false);
-        })
-        .catch((e) => {
+      })
+      .catch((e) => {
         toast.error(`${e.code}`);
         setLoading(false);
-        });
-    };
+      });
+  };
 
-    //preguntar si toda la informacion va en una sola tarjeta, 
-    //o para cada campo usar una tarjeta aparte
+  //preguntar si toda la informacion va en una sola tarjeta,
+  //o para cada campo usar una tarjeta aparte
 
-    //preguntar por el objeto como ponerlo en una constante
+  //preguntar por el objeto como ponerlo en una constante
 
-    //como usar el effect para cuando cambien el nombre, apellido o alguno de los campos
+  //como usar el effect para cuando cambien el nombre, apellido o alguno de los campos
 
+  return (
+    <DashboardLayout>
+      <DashboardTitle
+        title={user?.username}
+        breadcrumb={["", "Profile"]}
+        iconic="fa fa-home"
+        link="/"
+      ></DashboardTitle>
+      <div style={{ display: "flex" }}>
+        <DashboardCard>
+          <DashboardCardHeader title="Current Info Profile">
+            {!updateField ? (
+              <DashboardCardHeaderButton
+                iconClass="fa fa-wrench"
+                onClick={() => setUpdateField(true)}
+              />
+            ) : null}
+            {updateField ? (
+              <DashboardCardHeaderButton
+                iconClass="fa fa-id-card"
+                onClick={() => setUpdateField(false)}
+              />
+            ) : null}
+          </DashboardCardHeader>
+          <div className={styles.cardContainer}>
+            {!updateField ? (
+              <div className={styles.cardInfo}>
+                <div className={styles.row}>
+                  <h4>Name</h4>
+                  <div>{currentName}</div>
+                </div>
+                <div className={styles.row}>
+                  <h4>Lastname</h4>
+                  <div>{currentLastName}</div>
+                </div>
+                <div className={styles.row}>
+                  <h4>Birthdate</h4>
+                  <div>{currentBirthdate}</div>
+                </div>
+                <div className={styles.row}>
+                  <h4>Phone Number</h4>
+                  <div>{currentPhone}</div>
+                </div>
+              </div>
+            ) : null}
+            <div className={styles.form}>
+              {updateField ? (
+                <Formik
+                  initialValues={initialValues}
+                  validationSchema={validationSchema}
+                  onSubmit={onSubmit}
+                >
+                  {(formik) => {
+                    return (
+                      <Form className={styles.containerItems}>
+                        <FormikControl
+                          control="input"
+                          label="Name"
+                          type="name"
+                          name="name"
+                          placeholder={currentName}
+                        />
 
+                        <FormikControl
+                          control="input"
+                          label="Lastname"
+                          type="lastname"
+                          name="lastname"
+                          placeholder={currentLastName}
+                        />
 
-    return (
-        <DashboardLayout>
-            <DashboardTitle title={user?.username} breadcrumb={["", "Profile"]} iconic="fa fa-home" link="/"></DashboardTitle>
-            <div style={{'display':'flex'}}>
-                <DashboardCard>
-                    <DashboardCardHeader title="Current Info Profile">
-                        {!updateField?<DashboardCardHeaderButton iconClass="fa fa-wrench" onClick={() => setUpdateField(true)}/>:null}
-                        {updateField?<DashboardCardHeaderButton iconClass="fa fa-id-card" onClick={() => setUpdateField(false)}/>:null}
-                    </DashboardCardHeader>
-                    <div className={styles.cardContainer}>
-                    {!updateField?<div className={styles.cardInfo}>
-                        <div className={styles.row}>
-                            <h4>Name</h4>
-                            <div>{currentName}</div>
+                        <FormikControl
+                          control="input"
+                          label="Birthdate"
+                          type="date"
+                          name="birthdate"
+                          placeholder={currentBirthdate}
+                        />
+
+                        <FormikControl
+                          control="input"
+                          label="Phone"
+                          type="phone"
+                          name="phone"
+                          placeholder={currentPhone}
+                        />
+
+                        <div className={styles.buttonContainer}>
+                          <Button
+                            type="primary"
+                            htmlType="submit"
+                            disabled={!formik.isValid}
+                            size="large"
+                            loading={loading}
+                          >
+                            Send
+                          </Button>
+                          {!formik.isValid ? (
+                            <span className={styles.note}>
+                              COMPLETE THE FIELDS
+                            </span>
+                          ) : null}
                         </div>
-                        <div className={styles.row}>
-                            <h4>Lastname</h4>
-                            <div>{currentLastName}</div>
-                        </div>
-                        <div className={styles.row}>
-                            <h4>Birthdate</h4>
-                            <div>{currentBirthdate}</div>
-                        </div>
-                        <div className={styles.row}>
-                            <h4>Phone Number</h4>
-                            <div>{currentPhone}</div>
-                        </div>
-                        </div>:null}
-                        <div className={styles.form}>
-                        {updateField? <Formik
-                                initialValues={initialValues}
-                                validationSchema={validationSchema}
-                                onSubmit={onSubmit}
-                            >
-                            {(formik) => {
-                                return (
-                                <Form className={styles.containerItems}>
-                                    <FormikControl
-                                    control="input"
-                                    label="Name"
-                                    type="name"
-                                    name="name"
-                                    placeholder={currentName}
-                                    />
-                                
-                                    <FormikControl
-                                    control="input"
-                                    label="Lastname"
-                                    type="lastname"
-                                    name="lastname"
-                                    placeholder={currentLastName}
-                                    />
-
-                                    <FormikControl
-                                    control="input"
-                                    label="Birthdate"
-                                    type="date"
-                                    name="birthdate"
-                                    placeholder={currentBirthdate}
-                                    />
-
-                                    <FormikControl
-                                    control="input"
-                                    label="Phone"
-                                    type="phone"
-                                    name="phone"
-                                    placeholder={currentPhone}
-                                    />
-
-                                    <div className={styles.buttonContainer}>
-                                    <Button
-                                        type="primary"
-                                        htmlType="submit"
-                                        disabled={!formik.isValid}
-                                        size="large"
-                                        loading={loading}
-                                    >
-                                        Send
-                                    </Button>
-                                    {!formik.isValid ? (
-                                        <span className={styles.note}>COMPLETE THE FIELDS</span>
-                                    ) : null}
-                                    </div>
-                                </Form>
-                                );
-                            }}
-                            </Formik>:null}
-                        </div>
-                    </div>
-                </DashboardCard>
-
-                <DashboardCard>
-                    <DashboardCardHeader title="Hello">
-                        <DashboardCardHeaderButton iconClass="fa fa-bell" onClick={() => console.log("OK")}/>
-                        <DashboardCardHeaderButton iconClass="fa fa-bell" onClick={() => console.log("OK")}/>
-                    </DashboardCardHeader>
-                </DashboardCard>
+                      </Form>
+                    );
+                  }}
+                </Formik>
+              ) : null}
             </div>
+          </div>
+        </DashboardCard>
 
-        </DashboardLayout >
-    )
+        <DashboardCard>
+          <DashboardCardHeader title="Hello">
+            <DashboardCardHeaderButton
+              iconClass="fa fa-bell"
+              onClick={() => console.log("OK")}
+            />
+            <DashboardCardHeaderButton
+              iconClass="fa fa-bell"
+              onClick={() => console.log("OK")}
+            />
+          </DashboardCardHeader>
+        </DashboardCard>
+      </div>
+    </DashboardLayout>
+  );
 }
